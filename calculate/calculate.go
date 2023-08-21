798a511417780b85c0d36d85fdbb72cc7276bff1
yeesik.kim <yeesik.kim@nhn.com>
2023-08-21 11:56:56 +0900

    package test

---
+++ calculate/calculate.go
@@ -0,0 +1,9 @@
+package calculate
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+func Min(a, b int) int {
+	return a - b
+}
---
+++ printeo/printeo.go
@@ -0,0 +1,8 @@
+package printeo
+
+import "fmt"
+
+func PrintlnMyView(value string) {
+	fmt.Println("my printeo start v1")
+	fmt.Println("value is " + value)
+}
